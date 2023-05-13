7b03c92aee188004233639e9176007f7f36986e8
Ezra Dongijena <ezrakudzi@gmail.com>
2023-05-13 08:44:03 -0700

    file for prompting cmd

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* Filename: main.h
+* Authore: E Dongijena
+* Descr: A file which contains all prototypes for the simple_shell program
+*/
+
+#include <sys/types.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int prompt(char **buff);
+void processus(char **av, char *buf, ssize_t charactersnumbers);
+
+#endif
