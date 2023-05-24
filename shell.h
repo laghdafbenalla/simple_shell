e5d8aa3101c10c8967d21a0ff20d915c49db8a49
‘laghdaf <laghdaf.benalla@gmail.com>
2023-05-24 15:05:52 -0700

    upload

--- shell.h
+++ shell.h
@@ -236,14 +236,4 @@ int count_characters(char *string, char *character);
 
 /*======== alias_management.c ========*/
 
-/* print the list of alias */
-int print_alias(data_of_program *data, char *alias);
-
-/* get the alias name */
-char *get_alias(data_of_program *data, char *alias);
-
-/* set the alias name */
-int set_alias(char *alias_string, data_of_program *data);
-
-
 #endif /* SHELL_H */
